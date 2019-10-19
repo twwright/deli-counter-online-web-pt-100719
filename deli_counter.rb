84d12763720bf78548f645fef4c44c2c2f303430
Thomas Wright <twwright@gmail.com>
2019-10-19 21:01:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.count = 0
+  if katz_deli.count == 0
     puts "There is nobody waiting to be served!"
   else
   katz_deli.each do |person|
