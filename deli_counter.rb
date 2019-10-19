0f676088babe9077c9636c80190246f06cc41a25
Thomas Wright <twwright@gmail.com>
2019-10-19 21:01:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(katz_deli, name)
     katz_deli << name
-    puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1}."
+    puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
   katz_deli
 end
 
