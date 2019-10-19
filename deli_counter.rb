a6bb599cf2c20f259bcb144ba116fd19662f8d77
Thomas Wright <twwright@gmail.com>
2019-10-19 21:00:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,8 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.each do |new|
-    katz_deli << new
+    katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.count.with_index(1)}."
-  end
   katz_deli
 end
 
