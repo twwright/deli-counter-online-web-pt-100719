916e2780ceee2c01be8ec8a688fbf0f96efb5a46
Thomas Wright <twwright@gmail.com>
2019-10-19 20:25:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.count = 0
-    puts "There is nobody waiting to be server"!
+    puts "There is nobody waiting to be served!"
   else
   katz_deli.each do |person|
     puts "Currently serving #{person}."
