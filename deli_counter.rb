5b1954f015c722b1ea7cc7b6a97e8ea4d1941814
Thomas Wright <twwright@gmail.com>
2019-10-19 20:05:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  if katz_deli == 0
+    "The line is currently empty!"
+  else
+    currentline << name
+    puts "Welcome, #{name}. You are number #{{currentline.count.with_index(1)}}."
+  end
+end
+
+def
