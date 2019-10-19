352f9a3e0d9a21cd31701f25693300ed577ac6e2
Thomas Wright <twwright@gmail.com>
2019-10-19 20:21:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,19 @@
 katz_deli = []
 
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    orderedarr = katz_deli.map do |person|
+      katz_deli.index(person) + person
+    end
+    puts "The line is currently:" + orderedarr
+  end
+end
+
 def take_a_number(katz_deli, name)
-  katz_deli.each |new| do
+  katz_deli.each do |new|
     katz_deli.push(new)
     puts "Welcome, #{name}. You are number #{{katz_deli.count.with_index(1)}}."
   end
 end
-
-def line(katz_deli)
-  puts "The line is currently:" + katz_deli.join()
