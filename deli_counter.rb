4624fcd13b9e72919c29b2bb520dc1db492e4e50
Thomas Wright <twwright@gmail.com>
2019-10-19 20:08:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,9 @@
 katz_deli = []
 
 def take_a_number(katz_deli, name)
-  katz_deli.each do
-    currentline = []
-    currentline << name
-    puts "Welcome, #{name}. You are number #{{currentline.count.with_index(1)}}."
+  katz_deli.each |new| do
+    katz_deli.push(new)
+    puts "Welcome, #{name}. You are number #{{katz_deli.count.with_index(1)}}."
   end
 end
 
