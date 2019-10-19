c780ad46dcf269c7f8049596256e3bcd9a43a60f
Thomas Wright <twwright@gmail.com>
2019-10-19 20:45:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,8 @@ def line(katz_deli)
       "#{katz_deli.index(person)+1}" + ". " + person
     end
     puts "The line is currently: " + orderedarr.join(" ")
-    orderedarr
+    katz_deli = orderedarr
+    katz_deli
   end
 end
 
